07fb781683797a1bebb985a28f741acfb38b504d
matheusfis <matheus.fischer@sptech.school>
2025-03-02 21:01:13 -0300

    Adding json properties for PaymentStatus enum

--- src/main/java/sptech/school/enums/PaymentStatus.java
+++ src/main/java/sptech/school/enums/PaymentStatus.java
@@ -1,5 +1,8 @@
 package sptech.school.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PaymentStatus {
     PENDING("No payment has been made"),
     PARTIAL("50% payment made"),
@@ -12,8 +15,20 @@ public enum PaymentStatus {
         this.description = description;
     }
 
+    @JsonValue
     public String getDescription() {
         return description;
     }
+
+    @JsonCreator
+    public static PaymentStatus fromString(String value) {
+        for (PaymentStatus paymentStatus : PaymentStatus.values()) {
+            if (paymentStatus.name().equalsIgnoreCase(value)) {
+                return paymentStatus;
+            }
+        }
+        throw new IllegalArgumentException("Invalid payment status: " + value);
+    }
+
 }
 
